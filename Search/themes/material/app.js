f7d616af9e2d7f285478ffa7f54694e45836a61f
shillsincorporated <youknow@wheretofind.us>
2020-10-01 00:49:42 +0200

    why are you reading this

--- Search/themes/material/app.js
+++ Search/themes/material/app.js
@@ -767,13 +767,6 @@ function file_video(path) {
     .join('');
   player_items += `<li class="mdui-divider"></li>
                    <li class="mdui-menu-item"><a id="copy-link" class="mdui-ripple">Copy Link</a></li>`;
-  const playBtn = `
-      <button class="mdui-btn mdui-ripple mdui-color-theme-accent" mdui-menu="{target:'#player-items'}">
-        <i class="mdui-icon material-icons">&#xe039;</i>Play From External Player<i class="mdui-icon material-icons">&#xe5cf;</i>
-      </button>
-      
-      <ul class="mdui-menu" id="player-items">${player_items}</ul>`;
-
   const content = `
   
 <div class="mdui-container-fluid">
@@ -781,7 +774,7 @@ function file_video(path) {
 	<video class="mdui-video-fluid mdui-center" preload controls>
 	  <source src="${url}" type="video/mp4">
 	</video>
-	<br>${playBtn}
+	<br>
 	<!-- Fixed label -->
 	<div class="mdui-textfield">
 	  <label class="mdui-textfield-label">Download Link</label>
