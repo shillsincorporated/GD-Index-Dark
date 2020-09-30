7be561585a847201e0330da4779316bab71d639c
shillsincorporated <youknow@wheretofind.us>
2020-09-30 22:28:16 +0200

    why are you reading this

--- Search/themes/material/app.js
+++ Search/themes/material/app.js
@@ -359,7 +359,7 @@ function append_files_to_list(path, files) {
         p += "?a=view";
         c += " view";
       }*/
-      html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="${p}" class="${c}">
+      html += `<li class="mdui-list-item file mdui-ripple" target="_blank"><a gd-type="${item.mimeType}" href="/dl/${item.id}" class="${c}">
 	          <div class="mdui-col-xs-12 mdui-col-sm-7 mdui-text-truncate">
 	          <i class="mdui-icon material-icons">insert_drive_file</i>
 	            ${item.name}
