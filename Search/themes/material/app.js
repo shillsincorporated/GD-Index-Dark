c9529bfac4e737f17411bf1664595f033d207082
shillsincorporated <youknow@wheretofind.us>
2020-10-01 01:58:46 +0200

    why are you reading this

--- Search/themes/material/app.js
+++ Search/themes/material/app.js
@@ -754,10 +754,6 @@ function file_video(path) {
 	  <label class="mdui-textfield-label">Download Link</label>
 	  <input class="mdui-textfield-input" type="text" value="${url}"/>
 	</div>
-	<div class="mdui-textfield">
-	  <label class="mdui-textfield-label">HTML Refrence Adress</label>
-	  <textarea class="mdui-textfield-input"><video><source src="${url}" type="video/mp4"></video></textarea>
-	</div>
 </div>
 <a href="${url}" class="mdui-fab mdui-fab-fixed mdui-ripple mdui-color-theme-accent"><i class="mdui-icon material-icons">file_download</i></a>
 	`;
